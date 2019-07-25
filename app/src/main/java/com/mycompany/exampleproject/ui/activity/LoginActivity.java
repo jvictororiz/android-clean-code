ce0f2c209db7e893aca8ae856e37ab1f4aa9221b
jvictororiz <joao.victor.roriz2016@gmail.com>
2019-07-25 02:49:08 -0300

    Finalizando regras de login e password para validação do campos

--- app/src/main/java/com/mycompany/exampleproject/ui/activity/LoginActivity.java
+++ app/src/main/java/com/mycompany/exampleproject/ui/activity/LoginActivity.java
@@ -2,16 +2,13 @@ package com.mycompany.exampleproject.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.mycompany.exampleproject.R;
-import com.mycompany.exampleproject.data.request.UserLoginResponse;
 import com.mycompany.exampleproject.viewmodel.LoginViewModel;
 
 import java.util.Objects;
--- app/src/main/java/com/mycompany/exampleproject/viewmodel/LoginViewModel.java
+++ app/src/main/java/com/mycompany/exampleproject/viewmodel/LoginViewModel.java
@@ -3,6 +3,9 @@ package com.mycompany.exampleproject.viewmodel;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.mycompany.exampleproject.R;
+import com.mycompany.exampleproject.SuperApplication;
+import com.mycompany.exampleproject.Utils.TextUtils;
 import com.mycompany.exampleproject.data.request.UserLoginRequest;
 import com.mycompany.exampleproject.data.request.UserLoginResponse;
 import com.mycompany.exampleproject.repository.LoginRepository;
@@ -16,16 +19,39 @@ import retrofit2.Response;
 public class LoginViewModel extends BaseViewModel {
     private LoginRepository loginRepository = new LoginRepository();
     public MutableLiveData<UserLoginResponse> login = new MutableLiveData<>();
+    public MutableLiveData<String> loginMessageError = new MutableLiveData<>();
+    public MutableLiveData<String> passwordMessageError = new MutableLiveData<>();
 
     public void doLogin(String user, String password) {
         loading.setValue(true);
-        UserLoginRequest userLoginRequest = new UserLoginRequest();
-        userLoginRequest.setLogin(user);
-        userLoginRequest.setPassword(password);
-        UserLoginResponse responseData = getResponseData(loginRepository.doLogin(userLoginRequest));
-        if (responseData != null) {
-            login.setValue(responseData);
+        boolean fieldsValid = validateLogin(user) && validatePassword(password);
+        if (fieldsValid) {
+            UserLoginRequest userLoginRequest = new UserLoginRequest();
+            userLoginRequest.setLogin(user);
+            userLoginRequest.setPassword(password);
+            UserLoginResponse responseData = getResponseData(loginRepository.doLogin(userLoginRequest));
+            if (responseData != null) {
+                login.setValue(responseData);
+            }
         }
         loading.setValue(false);
+    }
+
+    private boolean validatePassword(String password) {
+        boolean validate = TextUtils.containsCapitalLetter(password)
+                && TextUtils.containsAlphaNumeric(password)
+                && TextUtils.containsSpecialCharacter(password);
+        if (!validate) {
+            loginMessageError.setValue(SuperApplication.getSuperApplication().getString(R.string.password_login));
+        }
+        return validate;
+    }
+
+    private boolean validateLogin(String user) {
+        boolean validate = TextUtils.isCpf(user) || TextUtils.isEmail(user);
+        if (!validate) {
+            loginMessageError.setValue(SuperApplication.getSuperApplication().getString(R.string.error_login));
+        }
+        return validate;
     }
 }
